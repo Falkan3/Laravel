6f24365e96376f58eb0314aefa77fc62b3c06e70
Adam Kocić <falkan333@gmail.com>
2016-03-10 13:15:57 +0100

    Revert "no message"
    This reverts commit 6dfc254d02dcad41cd3ba7e493fbf8cd65db77b9.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,70 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('hello/{name}', function($name){
+	echo 'Hello! How are you doing '.$name.'?';
+});
+
+Route::get('mypage',function(){
+	$data = array(
+		'var1' => 'Hamburger',
+		'var2' => 'Hot Dog',
+		'var3' => 'Fries'
+		);
+	return view('mypage', $data);
+});
+
+Route::get('orders', function(){
+	$orders = App\Order::all();
+	foreach($orders as $order){
+		//$customer = App\Customer::find($order->customer_id);
+		echo $order->name . " ordered by " . $order->customer->name . "<br />";
+	}
+});
+
+Route::get('customer/{id}', function($id){
+	$customer = App\Customer::find($id);
+	echo '<pre>';
+	echo ($customer->name . '<br />');
+	echo 'Orders: ' . '<br />';
+
+	echo '<ul>';
+	foreach($customer->orders as $order){
+		echo '<li>' . $order->name . '</li>';
+	}
+	echo '</ul>';
+});
+
+Route::get('customer_name', function(){
+	$customer = App\Customer::where('name','=','Tony')->first();
+	print_r($customer->id);
+});
+
+
+//read an item
+Route::get('test', function(){
+	echo '<form action="test" method="POST">';
+	echo '<input type="submit" value="submit">';
+	echo '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+	echo '<input type="hidden" name="_method" value="PUT">';
+	echo '</form>';
+});
+
+//create an item
+Route::post('test', function(){
+	echo 'POSTED';
+});
+
+//update an item
+Route::put('test', function(){
+	echo 'PUT';
+});
+
+//delete an item
+Route::delete('test', function(){
+	echo 'DELETED';
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
